bdc931e0bc768c01cf682e2eb533c564fd37e7cc
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-09-19 18:09:51 +0300

    fix api file

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
-if (!baseURL) {
-  throw new Error("NEXT_PUBLIC_API_URL is not defined");
-}
+// if (!baseURL) {
+//   throw new Error("NEXT_PUBLIC_API_URL is not defined");
+// }
 
 export const nextServer = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
