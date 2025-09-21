dbeb60e8ff2304f199a56e99fce1cca3abb0ee38
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-09-21 20:13:43 +0300

    test fix

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
   },
 };
 
-export const dynamic = "force-dynamic";
+//export const dynamic = "force-dynamic";
 
 export default async function ProfilePage() {
   const user = await getServerMe();
